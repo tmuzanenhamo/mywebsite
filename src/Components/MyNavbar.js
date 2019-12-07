6aa02295d726817eae5cdb145400a1e565833fb9
shamisolchikamhi <shamisolchikamhi@gmail.com>
2019-12-08 00:00:00 +0200

    Fixed some responsiveness

--- src/Components/MyNavbar.js
+++ src/Components/MyNavbar.js
@@ -7,35 +7,19 @@ class MyNavbar extends Component {
   render() {
     return (
 
- <div class="container">
-<nav class="navbar navbar-expand-lg fixed-top navbar-light">
-  <Link class="navbar-brand" to="/">Tawanda</Link>
-  <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item active">
-        <Link class="nav-link" to="/"> Home <span class="sr-only">(current)</span></Link>
-      </li>
-      <li class="nav-item">
-        <Link class="nav-link" to="/About">About</Link>
-      </li>
-      <li class="nav-item">
-        <Link class="nav-link" to="/Blog">Blog</Link>
-      </li>
-      <li class="nav-item">
-        <Link class="nav-link" to="/Projects">Projects</Link>
-      </li>
-      <li class="nav-item">
-        <Link class="nav-link" to="/Contact">Contact</Link>
-      </li>
-    </ul>
-  </div>
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+    <Link to="/" class="navbar-brand">Tawanda</Link>
+    
+        <div class="navbar-nav ml-auto">
+            <Link to="/" class="nav-item nav-link active">Home</Link>
+            <Link to="/About" class="nav-item nav-link">About</Link>
+            <Link to="/Blog" class="nav-item nav-link">Blog</Link>
+            <Link to="/Projects" class="nav-item nav-link " tabindex="-1">Projects</Link>
+            <Link to="/Contact" class="nav-item nav-link " tabindex="-1">Contact</Link>
+        </div>
 </nav>
 
-</div>
+
     );
   }
 }
