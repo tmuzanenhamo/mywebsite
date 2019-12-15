d1b88ce44690baf298b3d3a7ee982fe2412593a2
Tawanda <tawandamuzanenhamo14@gmail.com>
2019-12-15 19:22:30 +0200

    Updates

---
+++ precache-manifest.4b3a862a6315c3162b035e60d3d1f0ac.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c419fae1c87d7aaff9fcb7ef7dfd262",
+    "url": "/mywebsite/index.html"
+  },
+  {
+    "revision": "1898ae30763cc09942cf",
+    "url": "/mywebsite/static/css/2.1a739b2a.chunk.css"
+  },
+  {
+    "revision": "adaf6223a5faafaf5834",
+    "url": "/mywebsite/static/css/main.b2c559c2.chunk.css"
+  },
+  {
+    "revision": "1898ae30763cc09942cf",
+    "url": "/mywebsite/static/js/2.5dbed200.chunk.js"
+  },
+  {
+    "revision": "adaf6223a5faafaf5834",
+    "url": "/mywebsite/static/js/main.cd1ee909.chunk.js"
+  },
+  {
+    "revision": "598fce3c23fe940ed9fb",
+    "url": "/mywebsite/static/js/runtime-main.4bb7b1d2.js"
+  },
+  {
+    "revision": "6d4a4c1a80abce7be4b483bde1b3f000",
+    "url": "/mywebsite/static/media/Me.6d4a4c1a.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/mywebsite/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/mywebsite/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/mywebsite/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/mywebsite/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/mywebsite/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "e2117f2e7c0c0fe44c74d5ecbf8534e8",
+    "url": "/mywebsite/static/media/image22.e2117f2e.jpg"
+  },
+  {
+    "revision": "5dc5dfbe67150fa23c4036f88bfa2139",
+    "url": "/mywebsite/static/media/image3.5dc5dfbe.jpg"
+  },
+  {
+    "revision": "5690d807f8e7419d26e63e8133c68735",
+    "url": "/mywebsite/static/media/jcac.5690d807.jpg"
+  },
+  {
+    "revision": "24c3cae813f2d63d4df051fc96b21e4f",
+    "url": "/mywebsite/static/media/twiddle.24c3cae8.PNG"
+  },
+  {
+    "revision": "94287e247e679639c477b9793a549001",
+    "url": "/mywebsite/static/media/vs.94287e24.jpg"
+  }
+]);
