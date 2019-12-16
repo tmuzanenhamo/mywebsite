18762deed16621c6d6290b0f94e343c6bee6ca2d
Tawanda <tawandamuzanenhamo14@gmail.com>
2019-12-16 15:19:48 +0200

    Header update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,73 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import image3 from '../Images/image3.jpg';
 import '../App.css';
 import landingData from '../data/Data.json';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const validateText = (text) => {
+  // NOTE: Passes RFC 5322 but not tested on google's standard.
+  // eslint-disable-next-line no-useless-escape
+  const re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))$/;
+  return re.test(text) || text.length === 0;
+};
 
 
+const messages = [
+  'Artificial Intelligence',
+  'Telecommunications',
+  'Software Development ',
+  'Internet of Things',
+  'Embedded Engineering', 
+  'Electrical Engineering',
+  'Hardware Programming',
+  'Algorithm Design',
+  'Electronics Engineering',
+  'Cloud Computing',
+  'Web Development'
+];
+const useInterval = (callback, delay) => {
+  const savedCallback = useRef();
 
-export default class Header extends Component {
-  render() {
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    if (delay) {
+      const id = setInterval(() => {
+        savedCallback.current();
+      }, delay);
+      return () => clearInterval(id);
+    }
+    return () => {}; // pass linter
+  }, [delay]);
+};
+
+function Header() {
+
+  const hold = 50; // ticks to wait after message is complete before rendering next message
+  const delay = 50; // tick length in mS
+
+  const [idx, updateIter] = useState(0); // points to current message
+  const [message, updateMessage] = useState(messages[idx]);
+  const [char, updateChar] = useState(messages[idx].length); // points to current char
+  const [isActive, setIsActive] = useState(true); // disable when all messages are printed
+
+  useInterval(() => {
+    let newIdx = idx;
+    let newChar = char;
+    if (char - hold >= messages[idx].length) {
+      newIdx += 1;
+      newChar = 0;
+    }
+    if (newIdx === messages.length) {
+      setIsActive(false);
+    } else {
+      updateMessage(messages[newIdx].slice(0, newChar));
+      updateIter(newIdx);
+      updateChar(newChar + 1);
+    }
+  }, isActive ? delay : null);
     
     return (
 
@@ -17,10 +78,24 @@ export default class Header extends Component {
 
         {landingData.map((details)=> {
 
-          return <h1> Hi I'm {details.name}</h1>
+          return <h1 className = 'header-a'> {details.greeting} {details.name}</h1>
         })}
-        <p>I'm an Electrical and Computer Engineering student from the renowned University of Cape Town </p>
+        <p className ='header-a'>I'm an Electrical and Computer Engineering student from the renowned University of Cape Town<br/> 
+        </p>
+        <div
+          className="inline-container"
+          style={validateText(message) ? {} : { color: 'white' }}
+          onMouseEnter={() => setIsActive(false)}
+          onMouseLeave={() => (idx < messages.length) && setIsActive(true)}
+        >
 
+        <p className = 'header-a'><a className ='header-a' href={validateText(message) ? `mailto:${message}@tmuzanenhamo14@gmail.com` : ''}>
+        <span>My Areas of Interest includes: </span>
+              <span>{message}</span>
+              
+            </a>
+           </p>
+        </div>
       </div>
 
       </div>
@@ -28,5 +103,7 @@ export default class Header extends Component {
       </div>
      
     );
-  }
+
 }
+
+export default Header;
