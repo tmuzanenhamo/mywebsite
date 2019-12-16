0c49246ab9d0a29b77fc676d81d689e6d38cb86c
Tawanda <tawandamuzanenhamo14@gmail.com>
2019-12-16 16:42:32 +0200

    About Page Updated

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,27 +2,38 @@ import React from "react";
 import myFace from '../Images/image22.jpg'
 import '../App.css';
 import aboutData from '../data/Data.json';
+import Button from 'react-bootstrap/Button';
+import {Link} from 'react-router-dom';
 
 
 const About = () => {
 return (
-  <div class="wine-row">
-    <img src={myFace} alt=""></img>
-    <div class="wine-text-container">
-    {aboutData.map((data)=> {
-        return (
-        <div>
-        <h1 className ='heading'>{data.name}</h1>
-        <br></br>
-        <p className ="p">{data.bio} </p>
-        </div>
-            )
-          }
-       )
-    }     
+  <div class='some-page-wrapper'>
+  <div class='rows'>
+    <div class='columns'>
+      <div class=''>
+        <img src={myFace} alt=''/>
+      </div>
     </div>
+    <div class='columns'>
+      <div class=''>
+      {aboutData.map((data)=> {
+          return (
+          <div>
+          <h1 className ='parag'>{data.name}</h1>
+          <br></br>
+          <p className ="para">{data.bio} </p>
+          </div>
+              )
+            }
+        )
+        } 
+      </div>
+      <Button variant="outline-secondary"><Link to="/Resume">Contact Me</Link></Button>
+    </div>
+  </div>
 </div>
-
+      
 );
 }
 
