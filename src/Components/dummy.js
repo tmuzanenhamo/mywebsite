2087378d0e5cf1325edfdd6abba669ca2abfea30
tmuzanenhamo <tawandamuzanenhamo14@gmail,com>
2020-05-17 18:59:06 +0200

    Updated the about Page

---
+++ src/Components/dummy.js
@@ -0,0 +1,6 @@
+  title ={data.title}
+                link = {data.link}
+                date = {data.date}
+                id = {data.id}
+                image = {data.image}
+                description = {data.description}
